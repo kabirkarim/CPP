244ad96b6eab83920d08ea8a874d77a0509d0246
Kabir Karim <kabir.karim.kkd@gmail.com>
2026-02-04 11:43:06 +0500

    Q4 Added

---
+++ 1. Basic Logic/4_Area.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float r;
+    cout<<"Enter radius of circle: ";
+    cin>>r;
+    cout<<"Area = " << 3.142 * r * r<<endl;
+    cout<<"Circumference = " << 2 * 3.142 * r;
+ 
+    return 0;
+}
